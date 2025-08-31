d26f24f8ce9bc276ceded16289080509c484cdce
Juan Miguel  Arias Mejias <juanmiguelar09@gmail.com>
2025-08-30 23:24:57 -0600

    chore: add favicon links to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
           content:
             'Aprende a invertir desde cero con asesoría financiera personalizada. Logra tu retiro anticipado con la guía de un experto en inversiones.',
         },
+        { name: 'theme-color', content: '#ffffff' },
       ],
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -18,6 +19,11 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
         },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
       ],
     },
   },
